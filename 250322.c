e62767c058e880bcfe8e474c51c7f05506efb612
Richardlws <liu.bing66066@hotmail.com>
2025-03-23 06:42:24 +0800

    normal modify

---
+++ 250322.c
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+int main(){
+
+    //This is a comment blah
+    
+        This
+        is 
+        a
+        multiline
+        comment
+    
+    printf("I like Pizza!\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I like Pizza\" - Abraham Lincoln probably");
+
+    return 0;
+}
+*/
+
