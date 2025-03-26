0b9212590b9baa6562a4d7abd618bf6187156a06
Richardlws <liu.bing66066@hotmail.com>
2025-03-26 14:03:12 +0800

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    char grade;
+
+    printf("\nEnter a letter grade: ");
+    scanf("%c", &grade);
+
+    switch(grade){
+        case 'A':
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
