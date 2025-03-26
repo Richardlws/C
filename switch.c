8b0dab6993bef29ace9c999672848c1afa2c2c62
Richardlws <liu.bing66066@hotmail.com>
2025-03-26 14:11:55 +0800

    switchstatement

--- switch.c
+++ switch.c
@@ -9,23 +9,23 @@ int main(){
 
     switch(grade){
         case 'A':
-
-
+            printf("perfect!\n");
+            break;
+        case 'B':
+            printf("You did good!\n");
+            break;
+        case 'C':
+            printf("You did okay!\n");
+            break;
+        case 'D':
+            printf("At least it's not an F! \n");
+            break;
+        case 'F':
+            printf("You FAILED!\n");
+            break;
+        default:
+            printf("Please enter only valid grades");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
