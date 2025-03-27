699c7b2da5cff7253bb99605b0d0037f33289011
Richardlws <liu.bing66066@hotmail.com>
2025-03-27 14:53:27 +0800

    notlogic

---
+++ notlogical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    bool sunny = false;
+
+    if(!sunny){
+        printf("\nIt's cloudy outside!");
+    }
+    else{
+        printf("\nIt's sunny outside!");
+    }
+    return 0;
+}
