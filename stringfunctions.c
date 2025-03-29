37e3a5ba6cd65ce2f29f8dc9165aa5f1429459f0
Richardlws <liu.bing66066@hotmail.com>
2025-03-29 13:04:09 +0800

    stringfunction

---
+++ stringfunctions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char string1[] = "Bro";
+    char string2[] = "Code";
+
+    //strlwr(string1);
+    //strupr(string1);
+    //strcat(string1,string2);
+    //strncat(string1,string2,1);
+    //strcpy(string1, string2);
+    //strncpy(string1, string2,1); // copy n characters of string2 to string1
+    //strset(string1,'?'); //sets all charcters of a string to a geiven character
+    //strnset(string1,'x',1); //sets first n characters of a string to a given character
+    //strrev(string1); //reverses a string
+
+    //printf("%d", string1);
+
+    //int result = strlen(string1); //returns string length as int
+    //int result = strcmp(string1,string2); //string compare all characters
+    //int result = strncmp(string1, string2, 1); //string compare n characters
+    //int result = strcmpi(string1, string1); //string compare all (ignore case)
+    //int result = strnicmp(string1, string1,1); //string compare n characters(ignore case)
+
+    printf("%d", result);
+
+}
