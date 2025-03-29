a99cbc1666d34175f904e0ee17a506e7f09147ea
Richardlws <liu.bing66066@hotmail.com>
2025-03-29 22:36:39 +0800

    2darray

--- rray.c
+++ rray.c
@@ -4,19 +4,23 @@ int main()
 {
     // array = a data structure that can store many values of the same data type.
 
-    double prices[5];
+    double prices[6];
 
     prices[0] = 5.0;
     prices[1] = 10.0;
     prices[2] = 15.0;
-    prices[3] = 25.0;
-    prices[4] = 30.0;
+    prices[3] = 20.0;
+    prices[4] = 25.0;
+    prices[5] = 30.0;
+
+    // printf("%d bytes\n", sizeof(prices));
 
     char name[] = "Bro";
 
-    for (int i = 0; i <= 2; i++)
-        {
-            printf("%c", name[i],"\n");
-        }
+    for (int i = 0; i < sizeof(prices) / sizeof(prices[0]); i++)
+    {
+        printf("$%.2lf\n", prices[i]);
+    }
+
     return 0;
 }
