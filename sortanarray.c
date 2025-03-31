94bd5c4bf2eaf06ed708e0da88a6b68493a78519
Richardlws <liu.bing66066@hotmail.com>
2025-03-31 14:49:33 +0800

    sortanarray

--- sortanarray.c
+++ sortanarray.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
+void swapvalue(int array[],int i,int j)
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
 void sort(int array[], int size)
 {
     for (int i = 0; i < size - 1; i++)
     {
-        for (int j = 1; j < size; j++)
+        for (int j = i + 1; j < size; j++)
         {
-            printf("\ni de %dxun huan array[i]= %d",i,array[i]);
-            printf("\nj de %dxun huan array[j]= %d",j,array[j]);
-            
-            if (array[i] > array[j])
+            if (array[i] < array[j])
             {
-                int temp = array[j];
-                array[i] = array[j];
-                array[j] = temp;
+                swapvalue(array,i,j);
+                //int temp = array[i];
+                //array[i] = array[j];
+                //array[j] = temp;
             }
         }
-        printArray(array, size);
     }
 }
 
@@ -32,8 +36,7 @@ int main()
 
     int array[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
     int size = sizeof(array) / sizeof(array[0]);
-    // printf("\nsizeofarray is :%d", sizeof(array));
-    // printf("\nsizeofarray[] is : %d", sizeof(array[0]));
+
     sort(array, size);
     printArray(array, size);
 
