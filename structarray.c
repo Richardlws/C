a6b6cf0bbb08c577f4fa580e3c534b67a2b3a921
Richardlws <liu.bing66066@hotmail.com>
2025-04-01 16:49:59 +0800

    structarray

---
+++ structarray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[15];
+    float gpa;
+};
+
+int main()
+{
+    struct Student student1 = {"Spongebob",3.0};
+    struct Student student2 = {"Patrick",2.5};
+    struct Student student3 = {"Sandy",4.0};
+    struct Student student4 = {"Squidward",3.0};
+
+    struct Student students[] = {student1,student2,student3,student4};
+
+    //printf("%s",students[0].name);
+
+    for (int i = 0; i < sizeof(students)/sizeof(students[0]);i++)
+    {   
+        printf("Name:%s Gpa:%f\n", students[i].name, students[i].gpa);
+    }
+    
+    return 0;
+    
+}
+
