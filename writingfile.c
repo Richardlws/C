b9ad233de7c25c8cca912f87d515599974ccffb8
Richardlws <liu.bing66066@hotmail.com>
2025-04-02 09:57:32 +0800

    writingfile

--- writingfile.c
+++ writingfile.c
@@ -2,6 +2,22 @@
 
 int main()
 {
+    /*
+    FILE *pF = fopen("test.txt","a");
 
+    fprintf(pF,"\nSpongebob Squarepants");
+
+    fclose(pF);
     return 0;
+    */
+
+    if (remove("test.txt") == 0)
+    {
+        printf("That file was deleted successfully!");
+    }
+    else
+    {
+        printf("That file was NOT deleted!");
+
+    }
 }
