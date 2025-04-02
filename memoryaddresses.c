540e5a6284babfa446b406934cbd58d2791a145a
Richardlws <liu.bing66066@hotmail.com>
2025-04-02 09:13:32 +0800

    normal modify

--- memoryaddresses.c
+++ memoryaddresses.c
@@ -6,9 +6,9 @@ int main()
     // memeory block = a single unit (byte) within memory, used to hold some value(person)
     // memory address = the address of where a meimory block is located (house address)
 
-    double a = 'X';
-    double b = 'Y';
-    double c = 'Z';
+    char a = 'X';
+    char b = 'Y';
+    char c = 'Z';
 
     printf("%d bytes\n", sizeof(a));
     printf("%d bytes\n", sizeof(b));
@@ -18,5 +18,6 @@ int main()
     printf("%p\n", &b);
     printf("%p\n", &c);
 
+    //printf("指针大小: %zu 字节\n", sizeof(void*));
     return 0;
 }
